77cbec209ab06bafac9db1505484cc42a9c32fb0
Justin Beckwith <justin.beckwith@gmail.com>
2011-08-05 14:36:30 -0700

    added memcached for real this time

---
+++ MvcFun/Globals.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Enyim.Caching;
+
+namespace MvcFun
+{
+	public class Globals
+	{
+		protected static MemcachedClient _cache;
+		public static MemcachedClient Cache
+		{
+			get
+			{
+				if (_cache == null)
+					_cache = new MemcachedClient();
+				return _cache;
+			}
+		}		
+	}
+}
